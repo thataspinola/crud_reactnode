3c8bb9aa4761342b0ca9071038a4578d3e47650f
thataspinola <thata.arlekina@gmail.com>
2024-06-28 17:46:00 -0600

    pesquisar nos todos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import Todo from "./components/Todo";
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
 
 import './App.css';
 
@@ -27,6 +28,8 @@ function App() {
     },
 
   ]);
+
+  const [search, setSearch] = useState("");
 
   const addTodo = (text, category) => {
     const newTodos = [
@@ -57,9 +60,13 @@ function App() {
   return <div className="app">
     <h1>Lista de Tarefas</h1>
 
+    <Search search={search} setSearch={setSearch} />
+
     <div className="todo-list">
-      { todos.map((todo) => (
-        <Todo key={ todo.id } todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
+      { todos
+        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
+        .map((todo) => (
+          <Todo key={ todo.id } todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
       )) }
     </div>
     
