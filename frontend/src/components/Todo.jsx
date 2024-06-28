6b632c556c7ef4f073961240850b549a5c403fc4
thataspinola <thata.arlekina@gmail.com>
2024-06-28 17:38:58 -0600

    completar todo

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,6 +1,6 @@
-const Todo = ({ todo, removeTodo }) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
   return (
-    <div className="todo">
+    <div className="todo" style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
       <div className="content">
         <p>{todo.text}</p>
         <p className="category">
@@ -9,7 +9,7 @@ const Todo = ({ todo, removeTodo }) => {
       </div>
 
       <div>
-        <button className="complete">Completar</button>
+        <button className="complete" onClick={() => completeTodo(todo.id)}>Completar</button>
         <button className="remove" onClick={() => removeTodo(todo.id)}>Remover</button>
       </div>
     </div>
